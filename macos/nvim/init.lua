9564b1a0792dce9c9b5d60c376a90399cd696a87
Niccolo Lampa <niccololampa@Niccolos-MacBook-Pro.local>
2023-06-04 11:48:21 +0800

    update nvim.lua to only show status line in current buffer

--- macos/nvim/init.lua
+++ macos/nvim/init.lua
@@ -2,3 +2,15 @@ require('plugins')
 require('maps')
 require('highlights')
 require('base')
+
+
+-- hide cursorline when in another buffer
+vim.cmd([[
+augroup CursorLine
+    autocmd!
+    autocmd VimEnter * setlocal cursorline
+    autocmd WinEnter * setlocal cursorline
+    autocmd BufWinEnter * setlocal cursorline
+    autocmd WinLeave * setlocal nocursorline
+augroup END
+]])
